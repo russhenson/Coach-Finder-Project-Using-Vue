4a132884369ab4a3b80b1fbe92fb03c07e9ae063
russhenson <49965783+russhenson@users.noreply.github.com>
2022-04-13 16:28:41 +0800

    coach registration form done

---
+++ src/components/coaches/CoachForm.vue
@@ -0,0 +1,66 @@
+<template>
+    <form @submit.prevent="submitForm">
+        <div class="form-control">
+            <label for="firstname">Firstname</label>
+            <input type="text" name="firstname" id="firstname" v-model.trim="firstName">
+        </div>
+        <div class="form-control">
+            <label for="lastname">Lastname</label>
+            <input type="text" name="lastname" id="lastname" v-model.trim="lastName">
+        </div>
+        <div class="form-control">
+            <label for="description">Description</label>
+            <textarea name="description" id="description" rows="5" v-model.trim="description"></textarea>
+        </div>
+        <div class="form-control">
+            <label for="rate">Hourly Rate</label>
+            <input type="number" name="rate" id="rate" v-model.number="rate">
+        </div>
+        <div class="form-control">
+            <h3>Areas of Expertise</h3>
+            <div>
+                <input type="checkbox" id="frontend" value="frontend" v-model="areas">
+                <label for="frontend">Frontend Development</label>
+            </div>
+            <div>
+                <input type="checkbox" id="backend" value="backend" v-model="areas">
+                <label for="backend">backend Development</label>
+            </div>
+            <div>
+                <input type="checkbox" id="career" value="career" v-model="areas">
+                <label for="career">Career Advisory</label>
+            </div>
+        </div>
+        <base-button>Register</base-button>
+    </form>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            firstName: '',
+            lastName: '',
+            description: '',
+            rate: null,
+            areas: []
+        }
+    },
+    methods: {
+        submitForm() {
+            const formData = {
+                first: this.firstName,
+                last: this.lastName,
+                desc: this.description,
+                rate: this.rate,
+                areas: this.areas
+            }
+            console.log(formData)
+        }
+    },
+}
+</script>
+
+<style scoped src="../../styles/coach-form.css">
+
+</style>
