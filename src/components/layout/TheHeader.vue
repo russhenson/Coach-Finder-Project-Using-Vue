4fa598b468c2009f4a882fe7abf6f371cce584f1
russhenson <49965783+russhenson@users.noreply.github.com>
2022-04-13 13:58:43 +0800

    Main layout and header styling

---
+++ src/components/layout/TheHeader.vue
@@ -0,0 +1,14 @@
+<template>
+    <header>
+        <nav>
+            <h1><router-link to="/">Coach Finder</router-link></h1>
+            <ul>
+                <li><router-link to="/coaches">All Coaches</router-link></li>
+                <li><router-link to="/requests">Request</router-link></li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<style scoped src="../../styles/header.css">
+</style>
